5895eb516e7194813c9b691240efe9c10c6793cf
leeheajin <43198796+leeheajin@users.noreply.github.com>
2020-08-29 03:26:29 +0900

    Add files via upload

---
+++ HW6_1771046_이혜진/27p_SHIFT_Case2/SIFT-skeleton.cpp
@@ -0,0 +1,192 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/nonfree/nonfree.hpp>
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+#define RATIO_THR 0.4
+
+using namespace std;
+using namespace cv;
+
+double euclidDistance(Mat& vec1, Mat& vec2);
+int nearestNeighbor(Mat& vec, vector<KeyPoint>& keypoints, Mat& descriptors);
+void findPairs(vector<KeyPoint>& keypoints1, Mat& descriptors1,
+	vector<KeyPoint>& keypoints2, Mat& descriptors2,
+	vector<Point2f>& srcPoints, vector<Point2f>& dstPoints, bool crossCheck, bool ratio_threshold);
+
+int main() {
+
+	Mat input1 = imread("input1.jpg", CV_LOAD_IMAGE_COLOR);
+	Mat input2 = imread("input2.jpg", CV_LOAD_IMAGE_COLOR);
+	Mat input1_gray, input2_gray;
+
+	if (!input1.data || !input2.data)
+	{
+		std::cout << "Could not open" << std::endl;
+		return -1;
+	}
+
+	//resize(input1, input1, Size(input1.cols / 2, input1.rows / 2));
+	//resize(input2, input2, Size(input2.cols / 2, input2.rows / 2));
+
+	cvtColor(input1, input1_gray, CV_RGB2GRAY);
+	cvtColor(input2, input2_gray, CV_RGB2GRAY);
+
+	FeatureDetector* detector = new SiftFeatureDetector(
+		0,		// nFeatures
+		4,		// nOctaveLayers
+		0.04,	// contrastThreshold
+		10,		// edgeThreshold
+		1.6		// sigma
+	);
+
+	DescriptorExtractor* extractor = new SiftDescriptorExtractor();
+
+	// Create a image for displaying mathing keypoints
+	Size size = input2.size();
+	Size sz = Size(size.width + input1_gray.size().width, max(size.height, input1_gray.size().height));
+	Mat matchingImage = Mat::zeros(sz, CV_8UC3);
+
+	input1.copyTo(matchingImage(Rect(size.width, 0, input1_gray.size().width, input1_gray.size().height)));
+	input2.copyTo(matchingImage(Rect(0, 0, size.width, size.height)));
+	
+	// Compute keypoints and descriptor from the source image in advance
+	vector<KeyPoint> keypoints1;
+	Mat descriptors1;
+
+	detector->detect(input1_gray, keypoints1);
+	extractor->compute(input1_gray, keypoints1, descriptors1);
+	printf("input1 : %d keypoints are found.\n", (int)keypoints1.size());
+
+	vector<KeyPoint> keypoints2;
+	Mat descriptors2;
+
+	// Detect keypoints
+	detector->detect(input2_gray, keypoints2);
+	extractor->compute(input2_gray, keypoints2, descriptors2);
+
+	printf("input2 : %zd keypoints are found.\n", keypoints2.size());
+
+	for (int i = 0; i < keypoints1.size(); i++) {
+		KeyPoint kp = keypoints1[i];
+		kp.pt.x += size.width;
+		circle(matchingImage, kp.pt, cvRound(kp.size*0.25), Scalar(255, 255, 0), 1, 8, 0);
+	}
+
+	for (int i = 0; i < keypoints2.size(); i++) {
+		KeyPoint kp = keypoints2[i];
+		circle(matchingImage, kp.pt, cvRound(kp.size*0.25), Scalar(255, 255, 0), 1, 8, 0);
+	}
+
+	// Find nearest neighbor pairs
+	vector<Point2f> srcPoints1;
+	vector<Point2f> dstPoints1;
+	vector<Point2f> srcPoints2;
+	vector<Point2f> dstPoints2;
+	bool crossCheck = true;
+	bool ratio_threshold = false;
+	findPairs(keypoints1, descriptors1, keypoints2, descriptors2, srcPoints1, dstPoints1, crossCheck, ratio_threshold); // I1->I2
+	findPairs(keypoints2, descriptors2, keypoints1, descriptors1, srcPoints2, dstPoints2, crossCheck, ratio_threshold); // I2->I1
+	printf("%zd keypoints are matched.\n", srcPoints1.size()+srcPoints2.size());
+
+	// Draw line between nearest neighbor pairs
+	for (int i = 0; i < (int)srcPoints1.size(); ++i) {
+		Point2f pt1 = srcPoints1[i];
+		Point2f pt2 = dstPoints1[i];
+		Point2f from = pt1;
+		Point2f to = Point(size.width + pt2.x, pt2.y);
+		line(matchingImage, from, to, Scalar(0, 0, 255));
+	}
+
+	for (int i = 0; i < (int)srcPoints2.size(); ++i) {
+		Point2f pt1 = srcPoints2[i];
+		Point2f pt2 = dstPoints2[i];
+		Point2f from = pt1;
+		Point2f to = Point(size.width + pt2.x, pt2.y);
+		line(matchingImage, from, to, Scalar(0, 0, 255));
+	}
+
+	// Display mathing image
+	namedWindow("Matching");
+	imshow("Matching", matchingImage);
+
+	waitKey(0);
+
+	return 0;
+}
+
+/**
+* Calculate euclid distance
+*/
+double euclidDistance(Mat& vec1, Mat& vec2) {
+	double sum = 0.0;
+	int dim = vec1.cols;
+	for (int i = 0; i < 1; i++)
+		for (int j = 0; j< dim; j++)
+		{
+			uchar * a = vec1.ptr(i);
+			uchar * b = vec2.ptr(i);
+			sum += (a[j] - b[j]) *(a[j] - b[j]);
+		}
+
+	return sqrt(sum);
+}
+
+/**
+* Find the index of nearest neighbor point from keypoints.
+*/
+int nearestNeighbor(Mat& vec, vector<KeyPoint>& keypoints, Mat& descriptors) {
+	int neighbor = -1;
+	double minDist = 1e6;
+
+	for (int i = 0; i < descriptors.rows; i++) {
+		Mat v = descriptors.row(i);		// each row of descriptor
+		//	Fill the code
+		KeyPoint pt = keypoints[i];
+		double d = euclidDistance(vec, v);
+		if (d < minDist) {
+			minDist = d;
+			neighbor = i;
+		}
+	}
+
+	return neighbor;
+}
+
+/**
+* Find pairs of points with the smallest distace between them
+*/
+void findPairs(vector<KeyPoint>& keypoints1, Mat& descriptors1,
+	vector<KeyPoint>& keypoints2, Mat& descriptors2,
+	vector<Point2f>& srcPoints, vector<Point2f>& dstPoints, bool crossCheck, bool ratio_threshold) {
+	for (int i = 0; i < descriptors1.rows; i++) {
+		KeyPoint pt1 = keypoints1[i];
+		Mat desc1 = descriptors1.row(i);
+
+		int nn = nearestNeighbor(desc1, keypoints2, descriptors2);
+
+		// Refine matching points using ratio_based thresholding
+		if (ratio_threshold) {
+			//
+			//	Fill the code
+			//
+		}
+
+		// Refine matching points using cross-checking
+		if (crossCheck) {
+			//	Fill the code
+			KeyPoint pt = keypoints2[nn];
+			Mat desc = descriptors2.row(nn);
+			int n = nearestNeighbor(desc, keypoints1, descriptors1);
+			if (i != n) { // cross-checking
+				continue; // Not use this points
+			}
+		}
+		if (nn >= 0) {
+			KeyPoint pt2 = keypoints2[nn];
+			srcPoints.push_back(pt1.pt);
+			dstPoints.push_back(pt2.pt);
+		}
+	}
+}
---
+++ HW6_1771046_이혜진/Readme.txt
@@ -0,0 +1,17 @@
+Open-Software Project (01)
+Assignment6 : OSP-Lec08-Feature Descriptor
+
+ 필자는 Visual Stduio 2015와 Open CV 2.4 버전을 사용하였다. 
+제공된 소스코드에서 버전이 맞지 않아 변경된 추가적인 변수는 없다.
+
+--------------------------------------------------------------------------------------------------------------------------------
+
+1. 27p_SHIFT_Case
+   SHIFT를 주어진 skeleton 코드를 기반으로 구현하는 코드이다. cross-check와 ratio-based는 각각 true/false값을 갖고 있다.
+   이 값들을 조정함으로서 어떠한 방법으로 SHIFT를 구현할 것인지 설정할 수 있다.
+   자세한 코드와 설명은 techincal report에 있다.
+   
+2. 28p_Simple_Application (features 2D + homography to find a known object)
+   주어진 코드가 첨부된 사이트에 들어가서 해당 코드를 분석하는 과제이다.
+   결과를 살펴보면 찾고자하는 주어진 2d 이미지가 전체 이미지의 어느 부분에 존재하는지를 알 수 있다.
+   자세한 분석은 techincal report에 있다.
